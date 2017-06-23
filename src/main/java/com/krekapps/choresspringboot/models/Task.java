6d99f9f74ddba4e17eae1df69ce10f534f090a79
Erin K. Kennon <ekkennon@gmail.com>
2017-06-23 17:58:28 -0500

    ability to add due date to task

--- src/main/java/com/krekapps/choresspringboot/models/Task.java
+++ src/main/java/com/krekapps/choresspringboot/models/Task.java
@@ -1,10 +1,15 @@
 package com.krekapps.choresspringboot.models;
 
+import org.springframework.core.convert.ConversionFailedException;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by raefo on 22-Jun-17.
@@ -20,11 +25,16 @@ public class Task {
     @NotNull
     private String name;
 
+    private boolean isDue;
+
+    private Date dueDate;
+
     public Task() {
     }
 
     public Task(String name) {
         this.name = name;
+        isDue = false;
     }
 
     public int getId() {
@@ -37,5 +47,39 @@ public class Task {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean isDue() {
+        return isDue;
+    }
+
+    public void setDue(boolean due) {
+        isDue = due;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+/*
+    public void setDueDate(Date dueDate) {
+        isDue = true;
+        this.dueDate = dueDate;
+    }*/
+
+    public void setDueDate(String date) {
+        isDue = true;
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            dueDate = format.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            dueDate = null;
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            dueDate = null;
+        } catch (ConversionFailedException e) {
+            e.printStackTrace();
+            dueDate = null;
+        }
     }
 }
