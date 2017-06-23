af68711d343e108e754da973c759be122cc59222
Erin K. Kennon <ekkennon@gmail.com>
2017-06-23 08:12:06 -0500

    new feature to add tasks

--- src/main/java/com/krekapps/choresspringboot/controllers/TasksController.java
+++ src/main/java/com/krekapps/choresspringboot/controllers/TasksController.java
@@ -1,11 +1,17 @@
 package com.krekapps.choresspringboot.controllers;
 
 import com.krekapps.choresspringboot.models.Task;
+import com.krekapps.choresspringboot.models.data.TaskDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+import javax.validation.Valid;
 
 /**
  * Created by raefo on 22-Jun-17.
@@ -15,12 +21,30 @@ import java.util.ArrayList;
 @RequestMapping(value="tasks")
 public class TasksController {
 
+    @Autowired
+    private TaskDao taskDao;
+
     @RequestMapping(value="view")
     public String view(Model model) {
-        ArrayList<Task> list = new ArrayList<>();
-        list.add(new Task("hello"));
         model.addAttribute("title", "Task List");
-        model.addAttribute("fields", list);
+        model.addAttribute("tasks", taskDao.findAll());
         return "tasks/view";
+    }
+
+    @RequestMapping(value="add", method=RequestMethod.GET)
+    public String displayAdd(Model model) {
+        model.addAttribute("title", "Add Task");
+        model.addAttribute(new Task());
+        return "tasks/add";
+    }
+
+    @RequestMapping(value="add", method=RequestMethod.POST)
+    public String processAdd(Model model, @ModelAttribute @Valid Task task, Errors errors, @RequestParam int id) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Task");
+            return "tasks/add";
+        }
+
+        return "redirect:";
     }
 }
---
+++ src/test/java/com/krekapps/choresspringboot/task/CreateTaskTest.java
@@ -0,0 +1,7 @@
+package com.krekapps.choresspringboot.task;
+
+/**
+ * Created by raefo on 23-Jun-17.
+ */
+public class CreateTaskTest {
+}
